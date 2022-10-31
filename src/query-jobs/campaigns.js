f2c0d06941f1bf0d8aad05add252aeea8b9a08ab
cavis <ryancavis@gmail.com>
2022-10-31 08:46:59 -0600

    Start on campaign metadata

---
+++ src/query-jobs/campaigns.js
@@ -0,0 +1,28 @@
+/** @typedef {import('../index').ExportConfig} ExportConfig */
+
+/**
+ * @param {ExportConfig} config
+ */
+module.exports = async function main(config) {
+  const query = `
+    SELECT
+      id,
+      account_id,
+      advertiser_id,
+      name,
+      type,
+      rep_name,
+      sales_rep_name,
+      notes,
+      created_at,
+      updated_at
+    FROM ${process.env.BIGQUERY_DATASET}.campaigns
+    WHERE id IN (
+      SELECT DISTINCT campaign_id FROM ${process.env.BIGQUERY_DATASET}.flights
+      WHERE podcast_id IN (${config.podcastIds.join(', ')})
+    )
+  `;
+  const [queryJob] = await config.bigQueryClient.createQueryJob({ query });
+
+  return queryJob;
+};
