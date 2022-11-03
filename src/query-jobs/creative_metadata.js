7289925249f4b7e5db0ee36a0f4ba63f21c237bc
cavis <ryancavis@gmail.com>
2022-11-03 09:51:06 -0600

    Filter creatives to just those attached to flights

--- src/query-jobs/creative_metadata.js
+++ src/query-jobs/creative_metadata.js
@@ -1,1 +1,29 @@
-// TODO
+/** @typedef {import('../index').ExportConfig} ExportConfig */
+
+/**
+ * @param {ExportConfig} config
+ */
+module.exports = async function main(config) {
+  const query = `
+    SELECT
+      id,
+      account_id,
+      advertiser_id,
+      url,
+      mime_type,
+      file_size,
+      filename,
+      media_type,
+      created_at,
+      updated_at
+    FROM ${process.env.BIGQUERY_DATASET}.creatives
+    WHERE id IN (
+      SELECT DISTINCT creative_id
+      FROM ${process.env.BIGQUERY_DATASET}.flights, UNNEST(creative_ids) AS creative_id
+      WHERE podcast_id IN (${config.podcastIds.join(', ')})
+    )
+  `;
+  const [queryJob] = await config.bigQueryClient.createQueryJob({ query });
+
+  return queryJob;
+};
