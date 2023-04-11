e3a8bfcc9ba4765b521dcafdbed2ee1f14292a1e
Chris Kalafarski <chris@farski.com>
2023-04-10 21:09:16 -0400

    Lint extraction.js

--- src/extraction.js
+++ src/extraction.js
@@ -1,30 +1,30 @@
-const MakeCopies = require('./make_copies');
+const MakeCopies = require("./make_copies");
 
-const queryForDownloads = require('./query-jobs/downloads');
-const queryForEpisodeMetadata = require('./query-jobs/episode_metadata');
-const queryForGeoMetadata = require('./query-jobs/geo_metadata');
-const queryForImpressions = require('./query-jobs/impressions');
-const queryForPodcastMetadata = require('./query-jobs/podcast_metadata');
-const queryForUserAgentMetadata = require('./query-jobs/user_agent_metadata');
-const queryForAdvertiserMetadata = require('./query-jobs/advertiser_metadata');
-const queryForCampaignMetadata = require('./query-jobs/campaign_metadata');
-const queryForCreativeMetadata = require('./query-jobs/creative_metadata');
-const queryForFlightMetadata = require('./query-jobs/flight_metadata');
-const queryForPlacementMetadata = require('./query-jobs/placement_metadata');
+const queryForDownloads = require("./query-jobs/downloads");
+const queryForEpisodeMetadata = require("./query-jobs/episode_metadata");
+const queryForGeoMetadata = require("./query-jobs/geo_metadata");
+const queryForImpressions = require("./query-jobs/impressions");
+const queryForPodcastMetadata = require("./query-jobs/podcast_metadata");
+const queryForUserAgentMetadata = require("./query-jobs/user_agent_metadata");
+const queryForAdvertiserMetadata = require("./query-jobs/advertiser_metadata");
+const queryForCampaignMetadata = require("./query-jobs/campaign_metadata");
+const queryForCreativeMetadata = require("./query-jobs/creative_metadata");
+const queryForFlightMetadata = require("./query-jobs/flight_metadata");
+const queryForPlacementMetadata = require("./query-jobs/placement_metadata");
 
 // The values should never include an "*", or output files could get real weird
 const JOB_TYPES = {
-  DOWNLOADS: 'downloads',
-  EPISODE_METADATA: 'episode_metadata',
-  GEO_METADATA: 'geo_metadata',
-  IMPRESSIONS: 'impressions',
-  PODCAST_METADATA: 'podcast_metadata',
-  USER_AGENT_METADATA: 'user_agent_metadata',
-  ADVERTISER_METADATA: 'advertiser_metadata',
-  CAMPAIGN_METADATA: 'campaign_metadata',
-  CREATIVE_METADATA: 'creative_metadata',
-  FLIGHT_METADATA: 'flight_metadata',
-  PLACEMENT_METADATA: 'placement_metadata',
+  DOWNLOADS: "downloads",
+  EPISODE_METADATA: "episode_metadata",
+  GEO_METADATA: "geo_metadata",
+  IMPRESSIONS: "impressions",
+  PODCAST_METADATA: "podcast_metadata",
+  USER_AGENT_METADATA: "user_agent_metadata",
+  ADVERTISER_METADATA: "advertiser_metadata",
+  CAMPAIGN_METADATA: "campaign_metadata",
+  CREATIVE_METADATA: "creative_metadata",
+  FLIGHT_METADATA: "flight_metadata",
+  PLACEMENT_METADATA: "placement_metadata",
 };
 
 /** @typedef {import('./index').ExportConfig} ExportConfig */
@@ -39,12 +39,12 @@ function gcsObjectPrefix(config) {
     config.inputPrefix,
     // inputPrefix can include a trailing slash if it needs to, don't add one
     `${config.inclusiveRangeStart.toISOString()}-${config.exclusiveRangeEnd.toISOString()}`,
-    '/',
+    "/",
     +config.requestTime, // Make it easier to disambiguate multiple similar jobs
-    '/',
+    "/",
     config.requestId, // Provides a safe level of uniqueness
-    '/', // Should always end in a slash
-  ].join('');
+    "/", // Should always end in a slash
+  ].join("");
 }
 
 /**
@@ -99,19 +99,19 @@ module.exports = {
     console.log(
       JSON.stringify({
         QueryJob: { Type: extractionType, Metadata: queryJob.metadata },
-      }),
+      })
     );
 
     const queryMetadata = await new Promise((resolve, reject) => {
-      queryJob.on('complete', resolve);
-      queryJob.on('error', reject);
+      queryJob.on("complete", resolve);
+      queryJob.on("error", reject);
     });
 
     const bucketName = process.env.GCP_EXPORT_BUCKET;
 
     // All extract jobs use multi-file wildcard output
     const filename = `${extractionType}-*.ndjson.gz`;
-    const objectName = [gcsObjectPrefix(config), filename].join('');
+    const objectName = [gcsObjectPrefix(config), filename].join("");
 
     const [extractJob] = await config.bigQueryClient.createJob({
       configuration: {
@@ -121,16 +121,16 @@ module.exports = {
           // other files created by this function, or they may overwrite each
           // other
           destinationUri: `gs://${bucketName}/${objectName}`,
-          destinationFormat: 'NEWLINE_DELIMITED_JSON',
+          destinationFormat: "NEWLINE_DELIMITED_JSON",
           printHeader: true,
-          compression: 'GZIP',
+          compression: "GZIP",
         },
       },
     });
 
     await new Promise((resolve, reject) => {
-      extractJob.on('complete', resolve);
-      extractJob.on('error', reject);
+      extractJob.on("complete", resolve);
+      extractJob.on("error", reject);
     });
 
     // destinationUriFileCounts is an array where each value corresponds to
@@ -142,7 +142,7 @@ module.exports = {
     console.log(
       JSON.stringify({
         ExtractJob: { Type: extractionType, Metadata: extractJob.metadata },
-      }),
+      })
     );
 
     const copyMachines = [];
@@ -151,14 +151,14 @@ module.exports = {
     // copy however many were created.
     for (let i = 0; i < outputFileCount; i++) {
       // BigQuery makes files with 12-digit numbers, starting at 000000000000
-      const fileSequenceId = `${i}`.padStart(12, '0');
+      const fileSequenceId = `${i}`.padStart(12, "0");
 
       // Replace the wildcard in the filename we gave to BigQuery in the job
       // configuration with a 12-digit number, so it matches one of the files
       // that was created in GCS.
       const numberedObjectName = objectName.replace(
-        '-*.ndjson.gz',
-        `-${fileSequenceId}.ndjson.gz`,
+        "-*.ndjson.gz",
+        `-${fileSequenceId}.ndjson.gz`
       );
 
       // Copy that specific numbered file
@@ -168,8 +168,8 @@ module.exports = {
           config,
           bucketName,
           numberedObjectName,
-          fileSequenceId,
-        ),
+          fileSequenceId
+        )
       );
     }
 
