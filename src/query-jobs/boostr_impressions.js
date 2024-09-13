3ce149b6dbb5e11049989a3a3f1af946153dbfe1
cavis <ryancavis@gmail.com>
2024-09-13 14:23:37 -0600

    Linting

--- src/query-jobs/boostr_impressions.js
+++ src/query-jobs/boostr_impressions.js
@@ -4,14 +4,15 @@
  * @param {ExportConfig} config
  */
 export default async function job(config) {
+  const ds = process.env.BIGQUERY_DATASET;
   const query = `
     SELECT
       FORMAT_TIMESTAMP("%m/%d/%Y", i.timestamp) AS Date,
       COALESCE(x.external_id, f.external_id) AS \`Ad Server Line\`,
       COUNT(*) AS Impressions
-    FROM ${process.env.BIGQUERY_DATASET}.dt_impressions i
-    INNER JOIN ${process.env.BIGQUERY_DATASET}.flights f ON (i.flight_id = f.id)
-    LEFT JOIN ${process.env.BIGQUERY_DATASET}.flight_collection_external_ids x ON (f.id = x.flight_id AND i.feeder_podcast = x.podcast_id)
+    FROM ${ds}.dt_impressions i
+    INNER JOIN ${ds}.flights f ON (i.flight_id = f.id)
+    LEFT JOIN ${ds}.flight_collection_external_ids x ON (f.id = x.flight_id AND i.feeder_podcast = x.podcast_id)
     WHERE i.timestamp >= ?
       AND i.timestamp < ?
       AND i.is_duplicate = FALSE
