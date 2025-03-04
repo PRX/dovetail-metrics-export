77d7b35ecd947f0d3a870eb39ece4ce1a8ee0f6f
cavis <ryancavis@gmail.com>
2025-03-04 15:31:36 -0700

    Include child flight actuals

--- src/query-jobs/boostr_impressions.js
+++ src/query-jobs/boostr_impressions.js
@@ -6,18 +6,26 @@
 export default async function job(config) {
   const ds = process.env.BIGQUERY_DATASET;
   const query = `
+    WITH top_flights AS (
+      SELECT
+        f.id AS id,
+        COALESCE(p.id, f.id) AS top_id,
+        COALESCE(f.external_id, p.external_id) AS external_id,
+        COALESCE(f.integration_id, p.integration_id) AS integration_id
+      FROM ${ds}.flights f
+      LEFT JOIN ${ds}.flights p ON (f.parent_id = p.id)
+      WHERE COALESCE(f.integration_id, p.integration_id) IN (${config.integrationIds.join(", ")})
+    )
     SELECT
       FORMAT_TIMESTAMP("%m/%d/%Y", i.timestamp) AS Date,
-      COALESCE(x.external_id, f.external_id) AS \`Ad Server Line\`,
+      f.external_id AS \`Ad Server Line\`,
       COUNT(*) AS Impressions
     FROM ${ds}.dt_impressions i
-    INNER JOIN ${ds}.flights f ON (i.flight_id = f.id)
-    LEFT JOIN ${ds}.flight_collection_external_ids x ON (f.id = x.flight_id AND i.feeder_podcast = x.podcast_id)
+    INNER JOIN top_flights f ON (i.flight_id = f.id)
     WHERE i.timestamp >= ?
       AND i.timestamp < ?
       AND i.is_duplicate = FALSE
       AND i.creative_id IS NOT NULL
-      AND f.integration_id IN (${config.integrationIds.join(", ")})
     GROUP BY Date, \`Ad Server Line\`
     ORDER BY Date ASC, \`Ad Server Line\` ASC
   `;
